39b262283e7e8089de400f3804ecf72852ce8391
Rory Condict <rory.condict@gmail.com>
2026-09-22 22:20:05 +0100

    Update URL (#3)

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -1,6 +1,6 @@
 export const SITE = {
 	title: "rory condict.",
 	description: "a small collection of my things.",
-	url: "https://www.rorycondict.com",
+	url: "https://rorycondict.com",
 	image: "https://rorycondict.com/preview.webp",
 };
